9cd02f87bb8f2311c13864cdddd01ee7aa9ebb7e
Pratham Desai <prathamd94@gmail.com>
2019-11-17 13:22:02 -0800

    Fix bug in AbstractionRowBlock::getObject

--- presto-spi/src/main/java/io/prestosql/spi/block/AbstractRowBlock.java
+++ presto-spi/src/main/java/io/prestosql/spi/block/AbstractRowBlock.java
@@ -174,6 +174,10 @@ public abstract class AbstractRowBlock
         }
         checkReadablePosition(position);
 
+        if (isNull(position)) {
+            return null;
+        }
+
         return clazz.cast(new SingleRowBlock(getFieldBlockOffset(position), getRawFieldBlocks()));
     }
 
